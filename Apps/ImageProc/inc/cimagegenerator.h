d4d7cc524f66dfa5d1814ed0a7d20b0453e9f942
manid2 <maniyoung.d2@gmail.com>
2018-05-07 16:30:57 +0530

    added base img_props_dims_map

--- Apps/ImageProc/inc/cimagegenerator.h
+++ Apps/ImageProc/inc/cimagegenerator.h
@@ -12,7 +12,71 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+#include <map>
+
 namespace PFF {
+
+//! Resolutions
+enum eImgRes {
+  eResUnknown,
+  eRes100dpi = 100,
+  eRes150dpi = 150,
+  eRes200dpi = 200,
+  eRes300dpi = 300,
+  eRes400dpi = 400,
+  eRes600dpi = 600
+};
+
+//! Paper sizes
+enum ePaperSize {
+  ePSUnknown,
+  eA3,
+  eA4,
+  eA5,
+  eLT,
+  eLD,
+  eB4,
+  eB5
+};
+
+//! image properties
+struct stImgProps {
+  eImgRes res;
+  ePaperSize ps;
+
+  stImgProps()
+      : res(eResUnknown),
+        ps(ePSUnknown) {
+  }
+
+  stImgProps(ePaperSize p, eImgRes r)
+      : res(r),
+        ps(p) {
+  }
+
+  bool operator <(const stImgProps& p) const {
+    if (this->ps < p.ps && this->res < p.res)
+      return true;
+    else
+      return false;
+  }
+};
+
+//! image dimensions
+struct stImgDims {
+  int width;
+  int height;
+
+  stImgDims()
+      : width(0),
+        height(0) {
+  }
+
+  stImgDims(int w, int h)
+      : width(w),
+        height(h) {
+  }
+};
 
 /*
  * Generates a random image for testing
@@ -28,6 +92,8 @@ class CImageGenerator {
 
  public:
   cv::Mat m_image;
+  //! map of img props to img dims
+  std::map<stImgProps, stImgDims> img_props_dims_map;
 };
 
 using namespace cv;
@@ -39,30 +105,42 @@ static int execute(int argc, char **argv) {
   char wndname[] = "RND_Image";
   const int NUMBER = 100;
   const int DELAY = 0;
+  int lineType = CV_AA;
 
-  int lineType = CV_AA;  // change it to 8 to see non-antialiased graphics
-  int width = 1000, height = 700;
+  CImageGenerator imgGen;
+
+#if 1
+  imgGen.img_props_dims_map.insert(
+      std::make_pair(stImgProps(eA4, eRes200dpi), stImgDims(1646, 2331)));
+
+  int width = imgGen.img_props_dims_map[stImgProps(eA4, eRes200dpi)].width;
+  int height = imgGen.img_props_dims_map[stImgProps(eA4, eRes200dpi)].height;
+#else
+  int width = 1000;
+  int height = 700;
+#endif
+
   int x1 = -width / 2;
   int x2 = width * 3 / 2;
   int y1 = -height / 2;
   int y2 = height * 3 / 2;
+
   cv::RNG rng(0xFFFFFFFF);
-
   cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
-  do {
 
+  do {
     Point pt1, pt2;
     pt1.x = rng.uniform(x1, x2);
     pt1.y = rng.uniform(y1, y2);
     pt2.x = rng.uniform(x1, x2);
     pt2.y = rng.uniform(y1, y2);
 
-    CImageGenerator imgGen;
     cv::line(image, pt1, pt2, imgGen.getRandomColor(rng), rng.uniform(1, 10),
          lineType);
 
-    cv::imshow(wndname, image);
-    if (cv::waitKey(DELAY) >= 0) {
+    //cv::imshow(wndname, image);
+    //if (cv::waitKey(DELAY) >= 0)
+    {
       cv::imwrite("rnd_img_1.jpg", image);
       break;
     }
