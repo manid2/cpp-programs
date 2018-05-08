645efbf73520cdfa8170a9f9d9624d4747e6359e
manid2 <maniyoung.d2@gmail.com>
2018-05-08 13:51:14 +0530

    added enum2string

--- Apps/ImageProc/inc/cimagegenerator.h
+++ Apps/ImageProc/inc/cimagegenerator.h
@@ -12,8 +12,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-#include <map>
-
 namespace PFF {
 
 //! Resolutions
@@ -39,44 +37,14 @@ enum ePaperSize {
   eB5
 };
 
-//! image properties
-struct stImgProps {
-  eImgRes res;
-  ePaperSize ps;
+// TODO: make a map of `A4_200DPI_W` to its value
+// Constants to define
+static const int A4_200DPI_W = 1646;
+static const int A4_200DPI_H = 2331;
+static const int A4_300DPI_W = 2473;
+static const int A4_300DPI_H = 3497;
 
-  stImgProps()
-      : res(eResUnknown),
-        ps(ePSUnknown) {
-  }
-
-  stImgProps(ePaperSize p, eImgRes r)
-      : res(r),
-        ps(p) {
-  }
-
-  bool operator <(const stImgProps& p) const {
-    if (this->ps < p.ps && this->res < p.res)
-      return true;
-    else
-      return false;
-  }
-};
-
-//! image dimensions
-struct stImgDims {
-  int width;
-  int height;
-
-  stImgDims()
-      : width(0),
-        height(0) {
-  }
-
-  stImgDims(int w, int h)
-      : width(w),
-        height(h) {
-  }
-};
+#define VAR_NAME(x) #x
 
 /*
  * Generates a random image for testing
@@ -89,66 +57,23 @@ class CImageGenerator {
  public:
   cv::Scalar getRandomColor(cv::RNG& rng);
   void drawLine(void);
+  void drawRectangle(void);
+  void drawCircle(void);
+  void drawtext(const char* text);
+
+ private:
+  //std::string makeImgDim(void) const;
 
  public:
-  cv::Mat m_image;
-  //! map of img props to img dims
-  std::map<stImgProps, stImgDims> img_props_dims_map;
+  cv::Mat image;
+  ePaperSize paperSize;
+  eImgRes rsln;
 };
 
-using namespace cv;
+/*std::string CImageGenerator::makeImgDim(void) const {
+ }*/
 
-static int execute(int argc, char **argv) {
-  // pre-proc cmd line args
-  printf(" execute enter\n");
-
-  char wndname[] = "RND_Image";
-  const int NUMBER = 100;
-  const int DELAY = 0;
-  int lineType = CV_AA;
-
-  CImageGenerator imgGen;
-
-#if 1
-  imgGen.img_props_dims_map.insert(
-      std::make_pair(stImgProps(eA4, eRes200dpi), stImgDims(1646, 2331)));
-
-  int width = imgGen.img_props_dims_map[stImgProps(eA4, eRes200dpi)].width;
-  int height = imgGen.img_props_dims_map[stImgProps(eA4, eRes200dpi)].height;
-#else
-  int width = 1000;
-  int height = 700;
-#endif
-
-  int x1 = -width / 2;
-  int x2 = width * 3 / 2;
-  int y1 = -height / 2;
-  int y2 = height * 3 / 2;
-
-  cv::RNG rng(0xFFFFFFFF);
-  cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
-
-  do {
-    Point pt1, pt2;
-    pt1.x = rng.uniform(x1, x2);
-    pt1.y = rng.uniform(y1, y2);
-    pt2.x = rng.uniform(x1, x2);
-    pt2.y = rng.uniform(y1, y2);
-
-    cv::line(image, pt1, pt2, imgGen.getRandomColor(rng), rng.uniform(1, 10),
-         lineType);
-
-    //cv::imshow(wndname, image);
-    //if (cv::waitKey(DELAY) >= 0)
-    {
-      cv::imwrite("rnd_img_1.jpg", image);
-      break;
-    }
-  } while (0);
-
-  printf(" execute exit\n");
-  return 0;
-}
+extern int execute(int argc, char **argv);
 
 } /* namespace PFF */
 
