5e3f826e048620c63242eac7a177ee3f0a6d867d
manid2 <maniyoung.d2@gmail.com>
2018-07-31 23:54:03 +0530

    added hackerearth prob

--- Exps/main.cpp
+++ Exps/main.cpp
@@ -5,16 +5,18 @@
  *      Author: Mani Kumar
  */
 
-#include <lpdfcommon.h>
+//#include <lpdfcommon.h>
 
-#include <cstdio>
 
-#ifdef LPDF_BUILD_AS_EXE
+#include <competitiveprograms.h>
 
-using namespace LilPDF;
+//#ifdef LPDF_BUILD_AS_EXE
+
+//using namespace LilPDF;
+using namespace PFF;
 
 int main(int argc, char **argv) {
   return execute(argc, argv);
 }  // end
 
-#endif // LPDF_BUILD_AS_EXE
+//#endif // LPDF_BUILD_AS_EXE
