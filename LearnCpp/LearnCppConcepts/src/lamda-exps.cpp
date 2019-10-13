c5ce9117a7c739c7119d464b49527fe147d7615e
Mani Kumar <maniyoung.d2@gmail.com>
2019-10-13 21:25:19 +0530

    added enum and lamda exps code

---
+++ LearnCpp/LearnCppConcepts/src/lamda-exps.cpp
@@ -0,0 +1,21 @@
+/* Sample c++ code to show how to use lamdas in C++11
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/* Lamda syntax
+ *
+ * 1. [ captures ] ( params ) -> ret { body }
+ * 2. [ captures ] ( params ) { body }
+ * 3. [ captures ] { body }
+*/
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
