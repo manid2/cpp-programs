4da4c2b6760bbccd12c6c33cbba8823b5e8af501
Mani Kumar <maniyoung.d2@gmail.com>
2019-11-26 13:17:59 +0530

    added autotools support, closes #1

--- DesignPatterns/Builder/main.cpp
+++ DesignPatterns/Builder/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-//#include "cxmldombuilder.h"
 //#include "cburgerbuilder.h"
-//#include "carbuilder.h"
-#include "cxmlbuilder.h"
+#include "carbuilder.h"
 
 using namespace mani_dp;
 
