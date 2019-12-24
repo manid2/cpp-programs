51593b13096278651a88c901178094a81e4dccdc
Mani Kumar <maniyoung.d2@gmail.com>
2019-12-24 18:22:33 +0530

    added dummy test code for composite dp

---
+++ DesignPatterns/Composite/test/test_precomp.h
@@ -0,0 +1,14 @@
+/*
+ * test_precomp.h
+ *
+ *  Created on: 13-Jul-2019
+ *      Author: Mani Kumar
+ */
+
+#ifndef _DESING_PATTERNS_COMPOSITE__TEST_PRECOMP_H_
+#define _DESING_PATTERNS_COMPOSITE__TEST_PRECOMP_H_
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#endif /* _DESING_PATTERNS_COMPOSITE__TEST_PRECOMP_H_ */
